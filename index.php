e12a67bd368782a04aa41a5ec5e763453d9755b9
Yohannees <yohannesgetachewerieso@gmail.com>
2023-04-04 12:56:21 +0300

    first  files added

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>website</title>
+    <link rel="stylesheet" href="css/stylesheet.css">
+</head>
+<body color="white">
+    <section class="main">
+        <nav>
+            <a href="#" class="logo">
+                <img src="img/card.jpg" alt="...."  >
+        </nav>
+
+    </section>
+</body>
+</html>
