7570de4cca738a325d7edcd91db065c620c0ba6e
Yohannees <yohannesgetachewerieso@gmail.com>
2023-04-04 21:00:12 +0300

    stylesheet updated

--- index.php
+++ index.php
@@ -12,9 +12,15 @@
 <body color="color:white;">
     <section class="main">
         <nav>
-         <a href="#" class="logo" >
-            <img src="img/menu.png" alt="..."  style="width:100 px 100 px; "/>
-        </nav>
+            <a href="#" class="logo">
+                <img src="img/menu.png" alt="..." style="width:50px; height:50px; margin-top:20px; margin-left:20px;" />
+            </a>
+            <ul>
+                <li><a href="index.php" >home</a></li>
+                <li><a href="index.php" >features</a></li>
+                <li><a href="index.php" >about</a></li>
+                <li><a href="index.php" >contact-us</a></li>
+            </ul>
     </section>
 </body>
 
