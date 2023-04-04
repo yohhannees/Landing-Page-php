a618a89bb277473b392bd21bd6ce4f4b9881dde3
Yohannees <yohannesgetachewerieso@gmail.com>
2023-04-04 20:35:14 +0300

    updated

---
+++ index.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Website</title>
+    <link rel="stylesheet" href="css/stylesheet.css">
+</head>
+
+<body color="color:white;">
+    <section class="main">
+        <nav>
+         <a href="#" class="logo" >
+            <img src="img/menu.png" alt="..."  style="width:100 px 100 px; "/>
+        </nav>
+    </section>
+</body>
+
+</html>
